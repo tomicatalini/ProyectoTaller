570a301c146ec722a6b2e29b5e264bd81cc3d6ee
Fede Emens <emens.fede@gmail.com>
2019-05-21 15:45:40 -0300

    asd

---
+++ AppTerminal/AppTerminal/BaseDeDatos/RepocitorioCliente.cs
@@ -0,0 +1,26 @@
+﻿using AppTerminal.BaseDeDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTerminal
+{
+    class RepocitorioCliente
+    {
+        private List<Cliente> iLista;
+
+        public RepocitorioCliente()
+        {
+            this.iLista = new List<Cliente>();
+        }
+
+        public void AgregarCliente (Cliente pCliente)
+        {
+            this.iLista.Add(pCliente);
+        }
+
+
+    }
+}
